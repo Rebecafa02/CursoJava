79dc7a00c013ecc4be75b98187143b1ae02cb655
fa-rebeca <devrebecafa@gmail.com>
2024-08-14 11:58:32 -0300

    Conversor de Fahrenheit para Celsius

---
+++ src/Fundamentos/FahrenheitParaCelsius.java
@@ -0,0 +1,16 @@
+package Fundamentos;
+
+import java.util.Scanner;
+
+public class FahrenheitParaCelsius {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite o valor da temperatura para fahrenheit: ");
+        int fahrenheit = entrada.nextInt();
+        int celsius = (fahrenheit - 32) * 5/9;
+
+        System.out.printf("%dºF equivale a %dºC ", fahrenheit, celsius);
+
+        entrada.close();
+    }
+}
